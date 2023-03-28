d3e116443286274ab2fa120ced8f6678fd374c1e
Charlie Bayolima <100892646+nico6855@users.noreply.github.com>
2023-03-28 10:37:36 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let addToDo = document.getElementById('addToDoButton');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+// TO Do Button not defined 
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling'); 
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild();
+    })
+})
